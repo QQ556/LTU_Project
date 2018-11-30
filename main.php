6c47fa81985b6f05c8de9ed612f729143bfb71ac
QQ556 <a700mb0000@gmail.com>
2018-12-01 00:40:33 +0800

    1

---
+++ main.php
@@ -0,0 +1,172 @@
+<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+
+<style type="text/css">
+@import url(css.css);
+</style>
+<?php
+date_default_timezone_set('Etc/GMT-8');//使用台灣地區時間
+function getToday(){
+	$today = getdate();
+	date("Y/m/d H:i");  //日期格式化
+	$year=$today["year"]; //年 
+	$month=$today["mon"]; //月
+	$day=$today["mday"];  //日
+ 
+	if(strlen($month)=='1')$month='0'.$month;
+	if(strlen($day)=='1')$day='0'.$day;
+	$today="'".$year."-".$month."-".$day."%'";
+	//echo "今天日期 : ".$today;
+	return $today;
+}
+$passed=$_COOKIE["passed"];
+  /*  如果 cookie 中的 passed 變數不等於 TRUE
+      表示尚未登入網站，將使用者導向首頁 index.html	*/
+  if ($passed != "TRUE")
+  {
+    header("location:index.html");
+    exit();
+  }
+  else
+  {
+    require_once("dbtools.inc.php");
+    $id = $_COOKIE{"id"}; 
+     }  
+    $link = create_connection();
+        
+    //執行 SELECT 陳述式取得使用者資料
+    $sql = "SELECT * FROM users Where id = $id";
+    $result = execute_sql($link, "member", $sql);
+    $row = mysqli_fetch_assoc($result);
+    //開啟第二組 punch card資料庫
+    
+    $MAX = "select MAX(`clock_out`) from `punch card` where `clock_out` like ".getToday()." AND `id`=".$id;//末次打卡
+    $MIN = "select MIN(`clock_in`) from `punch card` where `clock_in` like ".getToday()." AND `id`=".$id;//首次打卡
+    $ALL = "select `clock_in` 上班, `clock_out` 下班 FROM `punch card` WHERE `id`=".$id." "." AND (`clock_in` like ".getToday()."OR`clock_out` like ".getToday().')';
+
+    $result = execute_sql($link, "member", $MAX);
+    $row2 = mysqli_fetch_assoc($result);
+
+    $result2 = execute_sql($link, "member", $MIN);
+    $row3 = mysqli_fetch_assoc($result2);
+
+    $result3 = execute_sql($link, "member", $ALL);
+    $row4 = mysqli_fetch_assoc($result3);
+    $total_records=mysqli_num_rows($result3);  // 取得記錄數
+    ?>
+    列印值 <?PHP print_r ($ALL);?><br>
+	資料數<?PHP print_r ($total_records);?>
+
+	
+<!doctype html>
+
+<html>
+	<head>
+	  <title>會員管理</title>
+	  	<script language="JavaScript">
+		function ShowTime(){
+		var NowDate=new Date();
+		var y=NowDate.getFullYear();
+		var mon=NowDate.getMonth();
+		var d=NowDate.getDate();
+		var h=NowDate.getHours();
+		var m=NowDate.getMinutes();
+		var s=NowDate.getSeconds();
+		nowtime =y+'年'+mon+'月'+d+'日 - '+h+'時'+m+'分'+s+'秒';
+		document.getElementById('showbox').innerHTML =y+'年'+mon+'月'+d+'日 - '+h+'時'+m+'分'+s+'秒';
+		setTimeout('ShowTime()',1000);
+		}
+		</script>
+	  <meta charset="utf-8">
+	</head>
+	<body onload="ShowTime()">
+	    <div id="Base_Visitor">
+		    <div class="Head">
+		    	<a class="Logo" ></a>
+		    	<div class="users">
+		    		<a class="user">
+		    			&emsp;歡迎光臨&emsp;&emsp;<font color="##0a4cff"><?php echo $row{"name"} ?></font>
+		    			<br>
+	          			&emsp;登入身分&emsp;&emsp;<font color="##0a4cff"><?php echo $row{"position"} ?></font>
+	          		</a>
+		    	</div>
+		    </div>
+		    <div align="right" style="background: #ffffff;height:20px;padding:8px">
+			    <ul class="drop-down-menu">
+				    <li><a href="#">管理功能<span class="arrow-indicator"><i class="fa fa-angle-down"></i></span></a>
+			            <ul>
+			                <li><a href="/" >營業管理</a>
+			                </li>
+			                <li><a href="Employee.php">員工管理</a>
+			                </li>
+			                <li><a href="Working_hours.php">工時管理</a>
+			                </li>
+			                <li><a href="/">公告管理</a>
+			                </li>
+			            </ul>
+		        	</li>
+		        	<li><a href="#">我的帳戶<span class="arrow-indicator"><i class="fa fa-angle-down"></i></span></a>
+			            <ul>
+			                <li><a href="modify.php" target="_top">修改我的資料</a>
+			                </li>
+			                <li><a href="punchcard_table.php?>">打卡明細</a>
+			                </li>
+			                <li><a href="logout.php">登出網站</a>
+			                </li>
+			            </ul>
+		        	</li>
+		        </ul>
+	        </div>
+
+		    <div class="Body">
+	        	<hr/>
+	        	<div id="punchcard" style="float:left;">
+		        	<div>
+			        	<h3>線上打卡系統</h3>
+		        		<div id="showbox" >
+		        		<!-- 系統時間 -->	
+		        		</div>
+		        		<br>
+		        		<div style="float:left;">
+		        			<a class="Item2" href="clock_in.php?code=<?php echo $row{"id"} ?>">上班</a>
+		        			<a class="Item3" href="clock_out.php?code=<?php echo $row{"id"} ?>">下班</a>
+		        		</div>
+			        </div>
+			        <div style="clear:both;">
+			        	<a class="Item4" href="punchcard_table.php">打卡明細</a>
+			        </div> 
+		        </div>
+		        <div style="margin-left: 30px;margin-top: 0px;font-style: 24px;float:left; ">
+		        		<?php 
+		        			echo "<h3>今天打卡紀錄</h3>";
+			        		echo("首次上班打卡 - ".$row3{"MIN(`clock_in`)"});
+			        		echo "<br>";
+			        		echo("末次下班打卡 - ".$row2{"MAX(`clock_out`)"});
+			        		echo("<hr/>");
+			        		echo "<h3>今天打卡明細</h3>";		
+		        		?>
+		        		<div class="box">
+　　						<?php
+							mysqli_data_seek($result3, 0); 
+							echo "---共".$total_records."筆記錄---"; 
+							for($i=1;$i<=mysqli_num_rows($result3);$i++)
+							{
+								$rs=mysqli_fetch_row($result3);
+								if($rs[0] == NULL)
+								{
+									echo("<BR>");
+									echo "第".$i."筆 - ";
+									echo($rs[1])."下班卡";
+								} 
+								else
+								{
+									echo("<BR>");
+									echo "第".$i."筆 - ";
+									echo($rs[0])."上班卡";
+								}
+							}
+						?>
+		        		</div>    
+				</div>
+		  	</div>
+		</body>
+	</html>
---
+++ modify-table.php
@@ -0,0 +1,270 @@
+
+<style type="text/css">
+@import url(css.css);
+tr:nth-child(even) {
+    background: #CCC
+}
+tr:nth-child(odd) {
+    background-color: #FAFAFA;
+}
+/*td:hover {
+    background-color: #E6FBFF;
+}*/
+tr, td {
+    border: 0px solid #666;
+}
+.post input[type=search]{
+    padding: 10px;
+    background: #fafafa;
+    border: 1px solid #ddd;
+    border-radius: 20px;
+    font-size: 1rem;
+    color: #111;
+}
+.post{
+    padding: 5px;
+    background: #cccccc;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    font-size: 1rem;
+    color: #111;
+}
+</style>
+<?php
+  //檢查 cookie 中的 passed 變數是否等於 TRUE 
+  $passed = $_COOKIE{"passed"};
+	
+  //如果 cookie 中的 passed 變數不等於 TRUE
+  //表示尚未登入網站，將使用者導向首頁 index.html
+  if ($passed != "TRUE")
+  {
+    header("location:index.html");
+    exit();
+  }
+	
+  //如果 cookie 中的 passed 變數等於 TRUE
+  //表示已經登入網站，取得使用者資料	
+  else
+  {
+    require_once("dbtools.inc.php");
+		
+    $id = $_COOKIE{"id"};
+	
+    //建立資料連接
+    $link = create_connection();
+				
+    //執行 SELECT 陳述式取得使用者資料
+    $code = $_GET["code"];
+    $sql = "SELECT * FROM users Where id = $code";
+    $result = execute_sql($link, "member", $sql);
+    $row = mysqli_fetch_assoc($result);  
+    $rs = $result->fetch_row();  
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>修改會員資料</title>
+    <meta charset="utf-8">
+    <script type="text/javascript">
+      function check_data()
+      {
+        if (document.myForm.password.value.length == 0)
+        {
+          alert("「使用者密碼」一定要填寫哦...");
+          return false;
+        }
+        if (document.myForm.password.value.length > 10)
+        {
+          alert("「使用者密碼」不可以超過 10 個字元哦...");
+          return false;
+        }
+        // if (document.myForm.re_password.value.length == 0)
+        // {
+        //   alert("「密碼確認」欄位忘了填哦...");
+        //   return false;
+        // }
+        // if (document.myForm.password.value != document.myForm.re_password.value)
+        // {
+        //   alert("「密碼確認」欄位與「使用者密碼」欄位一定要相同...");
+        //   return false;
+        // }
+        if (document.myForm.name.value.length == 0)
+        {
+          alert("您一定要留下真實姓名哦！");
+          return false;
+        }	
+        if (document.myForm.year.value.length == 0)
+        {
+          alert("您忘了填「出生年」欄位了...");
+          return false;
+        }
+        if (document.myForm.month.value.length == 0)
+        {
+          alert("您忘了填「出生月」欄位了...");
+          return false;
+        }	
+        if (document.myForm.month.value > 12 | document.myForm.month.value < 1)
+        {
+          alert("「出生月」應該介於 1-12 之間哦！");
+          return false;
+        }
+        if (document.myForm.day.value.length == 0)
+        {
+          alert("您忘了填「出生日」欄位了...");
+          return false;
+        }
+        if (document.myForm.month.value == 2 & document.myForm.day.value > 29)
+        {
+          alert("二月只有 28 天，最多 29 天");
+          return false;
+        }	
+        if (document.myForm.month.value == 4 | document.myForm.month.value == 6
+          | document.myForm.month.value == 9 | document.myForm.month.value == 11)
+        {
+          if (document.myForm.day.value > 30)
+          {
+            alert("4 月、6 月、9 月、11 月只有 30 天哦！");
+            return false;					
+          }
+        }	
+        else
+        {
+          if (document.myForm.day.value > 31)
+          {
+            alert("1 月、3 月、5 月、7 月、8 月、10 月、12 月只有 31 天哦！");
+            return false;					
+          }				
+        }
+        if (document.myForm.day.value > 31 | document.myForm.day.value < 1)
+        {
+          alert("出生日應該在 1-31 之間");
+          return false;
+        }	
+        myForm.submit();					
+      }
+    </script>			
+  </head>
+  <body>
+    <p align="center"><img src="modify.jpg"></p>
+    <form name="myForm" method="post" action="update.php">
+      <table border="2" align="center" bordercolor="#6666FF">
+       <tr> 
+          <td colspan="4" bgcolor="#6666FF" align="center"> 
+            <font color="#FFFFFF">人資欄位</font>
+          </td>
+          <tr bgcolor="#99FF99"> 
+          <td align="right">開通帳號</td>
+          <td colspan="3"><?php echo $row{"open account"} ?></td>
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td align="right">員工職位</td>
+          <td colspan="3">
+         <select name="position"value="<?php  $row{"position"} ?>" >
+             <option value="一般員工">一般員工</option>
+　            <option value="行政員工">行政員工</option>
+        </select>
+        </td>
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td align="right">到職日</td>
+          <td>
+          <input id="search" name="take_office_date" type="date" 
+          placeholder="<?php echo $row{"take_office_date"} ?>"
+          value="<?php echo $row{"take_office_date"} ?>" >
+          </td>
+          <td align="right">離職日</td>
+          <td>
+          <input id="search" name="leave_office_date" type="date" 
+          placeholder="<?php echo $row{"leave_office_date"} ?>" 
+          value="<?php echo $row{"leave_office_date"} ?>">
+          </td>
+        </tr>
+        <tr> 
+          <td colspan="4" bgcolor="#6666FF" align="center"> 
+            <font color="#FFFFFF">員工欄位</font>
+          </td>
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td align="right">員工編號：</td>
+          <td colspan="3"><?php echo $row{"id"} ?></td>
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td align="right">*使用者帳號：</td>
+          <td colspan="3"><?php echo $row{"account"} ?></td>
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td align="right" >*使用者密碼：</td>
+          <td colspan="3"> 
+            <input type="text" name="password" size="15" value="<?php echo $row{"password"} ?>">
+            (請使用英文或數字鍵，勿使用特殊字元)
+          </td>
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td align="right">*姓名：</td>
+          <td colspan="3"><input type="text" name="name" size="8" value="<?php echo $row{"name"} ?>"></td>
+        </tr>
+        
+        <tr bgcolor="#99FF99"> 
+          <td align="right">*所在門市：</td>
+          <td colspan="3"><input type="text" name="address" size="45" value="<?php echo $row{"Store"} ?>">(如果有調店，請聯絡資訊部)</td> 
+          </td>
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td align="right">*性別：</td>
+          <td colspan="3"> 
+            <input type="radio" name="sex" value="男" checked>男 
+            <input type="radio" name="sex" value="女">女
+          </td >
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td align="right">*生日：</td>
+          <td colspan="3">民國 
+            <input type="text" name="year" size="2" value="<?php echo $row{"year"} ?>">年 
+            <input type="text" name="month" size="2" value="<?php echo $row{"month"} ?>">月 
+            <input type="text" name="day" size="2" value="<?php echo $row{"day"} ?>">日
+          </td>
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td align="right">電話：</td>
+          <td colspan="3"> 
+            <input type="text" name="telephone" size="20" value="<?php echo $row{"telephone"} ?>">
+            (依照 (02) 2311-3836 格式 or (04) 657-4587)
+          </td>
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td align="right">行動電話：</td>
+          <td colspan="3"> 
+            <input type="text" name="cellphone" size="20" value="<?php echo $row{"cellphone"} ?>">
+            (依照 (0922) 302-228 格式)
+          </td>
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td align="right">地址：</td>
+          <td colspan="3"><input type="text" name="address" size="45" value="<?php echo $row{"address"} ?>"></td>
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td align="right">E-mail 帳號：</td>
+          <td colspan="3"><input type="text" name="email" size="30" value="<?php echo $row{"email"} ?>"></td>
+        </tr>
+       
+        <tr bgcolor="#99FF99"> 
+          <td align="right">備註：</td>
+          <td colspan="3"><textarea name="comment" rows="4" cols="45"><?php echo $row{"comment"} ?></textarea></td>
+        </tr>
+        <tr bgcolor="#99FF99"> 
+          <td colspan="4" align="CENTER"> 
+            <input type="button" value="修改資料" onClick="check_data()">
+            <input type="reset" value="重新填寫">
+          </td>
+        </tr>
+      </table>
+    </form>
+  </body>
+</html>
+<?php
+    //釋放資源及關閉資料連接
+    mysqli_free_result($result);
+    mysqli_close($link);
+  }
+?>
